69077eee758c55684a8690d698960dc55a72d0f8
calebetinosa <caleb20etinosa@gmail.com>
2025-05-26 11:51:03 +0100

    Made the buttons interactive

---
+++ scripts/contact.js
@@ -0,0 +1,6 @@
+export function submitQuery () {
+    document.querySelector ('.js-contact-submit')
+        .addEventListener ('click', () => {
+            alert (`Thank you for reaching out. I'll get back as soon an possible.`);
+        });
+};
---
+++ scripts/home.js
@@ -0,0 +1,13 @@
+import { submitQuery } from './contact.js';
+
+document.querySelector ('.js-cv-button')
+    .addEventListener ('click', () => {
+        alert ('Nothing to see yet');
+    });
+
+document.querySelector ('.js-explore-button')
+    .addEventListener ('click', () => {
+        alert ('Nothing to see here yet');
+    });
+
+submitQuery ();
