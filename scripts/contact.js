b37f9d0e0b004065da7be1afef8e0c2323d74034
calebetinosa <caleb20etinosa@gmail.com>
2025-06-23 11:20:15 +0100

    accepted incoming changes

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,10 +4,3 @@ export function submitQuery () {
             alert (`Thank you for reaching out. I'll get back as soon an possible.`);
         });
 }
-
-export function submitQuery () {
-    document.querySelector ('.js-contact-submit')
-        .addEventListener ('click', () => {
-            alert (`Thank you for reaching out. I'll get back as soon an possible.`);
-        });
-};
--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { submitQuery } from './contact.js';
 
 document.querySelector ('.js-cv-button')
@@ -11,18 +10,4 @@ document.querySelector ('.js-explore-button')
         alert ('Nothing to see here yet');
     });
 
-=======
-import { submitQuery } from './contact.js';
-
-document.querySelector ('.js-cv-button')
-    .addEventListener ('click', () => {
-        alert ('Nothing to see yet');
-    });
-
-document.querySelector ('.js-explore-button')
-    .addEventListener ('click', () => {
-        alert ('Nothing to see here yet');
-    });
-
->>>>>>> bd5c0b0f27c35ba8032e6b8ba2f498653f5d9e88
 submitQuery ();
